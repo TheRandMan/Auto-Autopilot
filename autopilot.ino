11336916d54d76ada6354fb51ac521903767d8ca
akicktothenads <33759959+TheRandMan@users.noreply.github.com>
2020-07-13 21:17:52 -0700

    Add files via upload

---
+++ autopilot.ino
@@ -0,0 +1,48 @@
+#include "DigiKeyboard.h"
+
+void setup() {
+    DigiKeyboard.delay(1500); //1.5 SECOND DELAY BEFORE BEGINNING
+    DigiKeyboard.sendKeyStroke(KEY_F10, MOD_SHIFT_LEFT); //F10 + SHIFT TO OPEN CMD
+    DigiKeyboard.delay(500);
+    DigiKeyboard.println("copy d:\\wifi.xml wifi.xml"); //COPY WIFI PROFILE FROM USB TO INTERNAL STORAGE
+    DigiKeyboard.delay(100);
+    DigiKeyboard.sendKeyStroke(KEY_ENTER); //ENTER KEY
+    DigiKeyboard.delay(100);
+    DigiKeyboard.println("netsh wlan add profile filename=wifi.xml"); //ADD WIFI PROFILE
+    DigiKeyboard.delay(1000);
+    DigiKeyboard.println("del apWCSD.xml"); //DELETE WIFI PROFILE XML FROM INTERNAL STORAGE
+    DigiKeyboard.sendKeyStroke(KEY_ENTER); //ENTER
+    DigiKeyboard.delay(100);
+    DigiKeyboard.println("exit"); //EXIT CMD
+    DigiKeyboard.delay(100);
+    DigiKeyboard.sendKeyStroke(KEY_ENTER); //ENTER
+    DigiKeyboard.delay(100);
+    DigiKeyboard.sendKeyStroke(43); //TAB
+    DigiKeyboard.delay(100);
+    DigiKeyboard.sendKeyStroke(43); //TAB
+    DigiKeyboard.delay(1500);
+    DigiKeyboard.sendKeyStroke(KEY_ENTER); //ENTER
+    DigiKeyboard.delay(3000);
+    DigiKeyboard.sendKeyStroke('r',MOD_GUI_LEFT); //WINDOWS KEY
+    DigiKeyboard.delay(100);
+    DigiKeyboard.sendKeyStroke('r',MOD_GUI_LEFT); //WINDOWS KEY
+    DigiKeyboard.delay(100);
+    DigiKeyboard.sendKeyStroke('r',MOD_GUI_LEFT); //WINDOWS KEY
+    DigiKeyboard.delay(100);
+    DigiKeyboard.sendKeyStroke('r',MOD_GUI_LEFT); //WINDOWS KEY
+    DigiKeyboard.delay(100);
+    DigiKeyboard.sendKeyStroke('r',MOD_GUI_LEFT); //WINDOWS KEY
+    DigiKeyboard.delay(3000);
+    DigiKeyboard.sendKeyStroke(KEY_DOWN_ARROW); //DOWN ARROW
+    DigiKeyboard.delay(100);
+    DigiKeyboard.sendKeyStroke(KEY_SPACE); //SPACE BAR
+    DigiKeyboard.delay(100);
+    DigiKeyboard.sendKeyStroke(43); //TAB
+    DigiKeyboard.delay(100);
+    DigiKeyboard.sendKeyStroke(43); //TAB
+    DigiKeyboard.delay(100);
+    DigiKeyboard.sendKeyStroke(KEY_ENTER); //ENTER
+    }
+
+void loop(){
+}
